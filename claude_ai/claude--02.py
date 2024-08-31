e1d13d79b3da4af362cd4188c03667c5ddf107a6
kwishna <kwishna@gmail.com>
2024-09-01 02:29:25 +0530

    claude 2

---
+++ claude_ai/claude--02.py
@@ -0,0 +1,58 @@
+import anthropic
+import os
+import base64
+import httpx
+
+client = anthropic.Anthropic(
+    # os.environ.get("ANTHROPIC_API_KEY")
+    api_key=os.environ.get("ANTHROPIC_API_KEY"),
+)
+
+image1_url = "https://upload.wikimedia.org/wikipedia/commons/a/a7/Camponotus_flavomarginatus_ant.jpg"
+image1_media_type = "image/jpeg"
+image1_data = base64.b64encode(httpx.get(image1_url).content).decode("utf-8")
+
+image2_url = "https://upload.wikimedia.org/wikipedia/commons/b/b5/Iridescent.green.sweat.bee1.jpg"
+image2_media_type = "image/jpeg"
+image2_data = base64.b64encode(httpx.get(image2_url).content).decode("utf-8")
+
+message = client.messages.create(
+    model="claude-3-5-sonnet-20240620",
+    max_tokens=1024,
+    system="Respond only in Spanish.",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Image 1:"
+                },
+                {
+                    "type": "image",
+                    "source": {
+                        "type": "base64",
+                        "media_type": image1_media_type,
+                        "data": image1_data,
+                    },
+                },
+                {
+                    "type": "text",
+                    "text": "Image 2:"
+                },
+                {
+                    "type": "image",
+                    "source": {
+                        "type": "base64",
+                        "media_type": image2_media_type,
+                        "data": image2_data,
+                    },
+                },
+                {
+                    "type": "text",
+                    "text": "How are these images different?"
+                }
+            ],
+        }
+    ],
+)
